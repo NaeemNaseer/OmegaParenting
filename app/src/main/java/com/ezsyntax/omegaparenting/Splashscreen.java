55e7656ccd64109181e775a4d1fc4e116f369eb5
Naeem Naseer <naeemnaseer99@gmail.com>
2016-10-30 04:35:37 +0500

    implemented login auth screens

--- app/src/main/java/com/ezsyntax/omegaparenting/Splashscreen.java
+++ app/src/main/java/com/ezsyntax/omegaparenting/Splashscreen.java
@@ -51,8 +51,7 @@ public class Splashscreen extends Activity {
                         sleep(100);
                         waited += 100;
                     }
-                    Intent intent = new Intent(Splashscreen.this,
-                            MainActivity.class);
+                    Intent intent = new Intent(Splashscreen.this, LoginActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(intent);
                     Splashscreen.this.finish();
